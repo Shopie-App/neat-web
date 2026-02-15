50a9c55bbfafebff732a0a88e00a06ca43b8fc85
Steve Armenakis <support@shopie.gr>
2026-02-15 05:04:50 +0200

    updates for neat-core

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -7,8 +7,6 @@ namespace App\Controllers;
 use Neat\Attributes\Http\HttpGet;
 use Neat\Http\ActionResult\ActionResult;
 
-use function Neat\Http\Status\OK;
-
 class IndexController
 {
     public function __construct()
